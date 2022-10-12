5d3ae2cbf40891fc64badbdf21672244a3f5b30f
¨Tomas <tomasgabriel1999@hotmail.com>
2022-10-11 23:31:21 -0300

    agregamos ultimos detalles

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,53 +1,50 @@
-import {Container, Row, Col, Table} from "react-bootstrap";
-const Formulario = ({ infoClima }) => {
-  const { coord, weather, main, sys, name } = { ...infoClima };
-  return ( 
-  <>
-  <Container>
-  <Row>
-  <Col md={8}>
-  <Table striped bordered hover size ="sm">
-      <tbody>
-        <tr>
-          <td>Localidad</td>
-          <td>{name}</td>
-        </tr>
-        <tr>
-          <td>Pais</td>
-          <td>{sys.country}</td>
-        </tr>
-        <tr>
-        <td>Coordenadas</td>
-        <td> Longitud: {coord.lon} Latitud: {coord.lat}
-        </td>
-        </tr>
-        <tr>
-        <td>Pronostico</td>
-        <td>{weather[0].description}</td>
-        </tr>
-        <tr>
-        <td>Temperatura</td>
-        <td>{main.temp}</td>
-        </tr>
-        <tr>
-        <td>La Maxima</td>
-        <td>{main.temp_max}</td>
-        </tr>
-       <tr>
-      <td>La Minima</td>
-      <td>{main.temp_min}</td>
-      </tr>
-      <tr>
-     <td>Sensacion Tèrmica</td>
-    <td>{main.feels_like}</td>
-    </tr>
-      </tbody>
-    </Table>
-  </Col>
-  </Row>
-  </Container>
-  
-  </> );
-}
- 
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import Informacion from "./Informacion";
+
+const Formulario = () => {
+  const [datosclima, setDatosClima] = useState();
+  const [ciudad, setCiudad] = useState("");
+
+  const consultarAPI = async () => {
+    try {
+      let respuesta = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=$&APPID=370fd6c1a4ad7f1281fc4876c646d91d"
+      );
+      const respuestaClima = await respuesta.json();
+      return respuestaClima;
+    } catch {}
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setCiudad(ciudad.trim());
+    console.log(ciudad);
+    consultarAPI();
+  };
+
+  useEffect(() => {
+    if (ciudad.trim().length > 0) {
+      consultarAPI().then((respuestaDatosClima) => {
+        setDatosClima(respuestaDatosClima);
+      });
+    }
+  }, [ciudad]);
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col md={6}>
+            <Form className="p-2 my-3">
+              <Button type="submit">Buscar</Button>
+            </Form>
+          </Col>
+        </Row>
+        <Informacion />
+      </Container>
+    </>
+  );
+};
+
 export default Formulario;
--- src/components/informacion.jsx
+++ src/components/informacion.jsx
@@ -1,80 +1,53 @@
-import { useEffect, useState } from "react";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import "react-bootstrap";
+import { Container, Row, Col, Table } from "react-bootstrap";
 
-
-
-const informacion = () => {
-  //datos guardados en el imput
-  const [ciudad, setCiudad] = useState("");
-  //peticion a la api
-  const [local, setLocal] = useState("");
-  //variables acerca del clima con la api
-  const [infoClima, setInfoClima] = useState({
-    coord: {},
-        weather: [{ id: "", main: "", description: "", icon: "" }],
-        base: "",
-        main: { temp: "", feels_like: "", temp_min: "", temp_max: "", pressure: "", humidity: "" },
-        visibility: "",
-        wind: { speed: "", deg: "" },
-        clouds: { all: "" },
-        dt: "",
-        sys: { type: "", id: "", country: "", sunrise: "", sunset: "" },
-        timezone: "",
-        id: "",
-        nombre: "",
-        cod: "",
-  });
-  
-  useEffect(() => {
-    if (ciudad.trim().length > 0) {
-        consultarAPI().then((respuestaDatosClima) => {
-            setDatosClima(respuestaDatosClima);
-        });
-    }
-}, [ciudad]);
-
-const consultarAPI = async () => {
-  try {
-      let respuesta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&APPID=370fd6c1a4ad7f1281fc4876c646d91d`);
-      const respuestaClima = await respuesta.json();
-      return respuestaClima;
-  } catch (error) {
-      console.log(error);
-  }
-};
-const handleSubmit = (e) => {
-  e.preventDefault();
-  setCiudadOk(ciudad.trim());
-  console.log(ciudadOk);
-  consultarAPI();
-};
-  
-  
-return (
-  <>
+const Informacion = ({ datosClima }) => {
+  return (
+    <>
       <Container>
-          <Row>
-              <Col md={6}>
-                  <Form className="p-2 my-3" onSubmit={handleSubmit}>
-                      <Form.Control
-                          name="ciudad"
-                          value={ciudad}
-                          placeholder="Buscar ciudad"
-                          aria-label="Recipient's username"
-                          aria-describedby="basic-addon2"
-                          onChange={(e) => {
-                              setCiudad(e.target.value);
-                          }}
-                      />
-                      <Button type="submit">Buscar</Button>
-                  </Form>
-              </Col>
-          </Row>
-          <Informacion datosClima={datosClima} />
+        <Row>
+          <Col md={8}>
+            <Table striped bordered hover size="sm">
+              <tbody>
+                <tr>
+                  <td>Ciudad</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>Pais</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>Coordenadas</td>
+                  <td>Longitud: Latitud:</td>
+                </tr>
+                <tr>
+                  <td>Pronostico del Clima</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>Temperatura</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>Temperatura Max.</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>Temperatura Min.</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td>Sensacion Tèrmica</td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
       </Container>
-  </>
-);
+    </>
+  );
 };
 
- 
-export default informacion;
+export default Informacion;
